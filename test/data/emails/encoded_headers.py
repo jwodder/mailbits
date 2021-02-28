bbb56f127305dafbb9e699a4bad4bd4b2fc22816
John T. Wodder II <git@varonathe.org>
2021-02-28 14:49:34 -0500

    Test of encoded headers

---
+++ test/data/emails/encoded_headers.py
@@ -0,0 +1,26 @@
+data = {
+    "headers": {
+        "from": [
+            {
+                "display_name": "Keith Moore",
+                "address": "moore@cs.utk.edu",
+            },
+        ],
+        "to": [
+            {
+                "display_name": "Keld Jørn Simonsen",
+                "address": "keld@dkuug.dk",
+            },
+        ],
+        "cc": [
+            {
+                "display_name": "André Pirard",
+                "address": "PIRARD@vm1.ulg.ac.be",
+            },
+        ],
+        "subject": "If you can read this you understand the example.",
+    },
+    "preamble": None,
+    "content": "Taken from RFC 2047, section 8.\n",
+    "epilogue": None,
+}
