385cd960afffcabc5cac4c19fcd209da350923fc
John T. Wodder II <git@varonathe.org>
2021-02-28 14:34:43 -0500

    Tests of Reply-To and Resent-* headers

---
+++ test/data/emails/reply_to.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta, timezone
+
+data = {
+    "headers": {
+        "from": [
+            {
+                "realname": "Mary Smith",
+                "address": "mary@example.net",
+            },
+        ],
+        "to": [
+            {
+                "realname": "John Doe",
+                "address": "jdoe@machine.example",
+            },
+        ],
+        "reply-to": [
+            {
+                "realname": "Mary Smith: Personal Account",
+                "address": "smith@home.example",
+            },
+        ],
+        "subject": "Re: Saying Hello",
+        "date": datetime(1997, 11, 21, 10, 1, 10, tzinfo=timezone(timedelta(hours=-6))),
+        "message-id": "<3456@example.net>",
+        "in-reply-to": [
+            "<1234@local.machine.example>",
+        ],
+        "references": [
+            "<1234@local.machine.example>",
+        ],
+    },
+    "preamble": None,
+    "content": "This is a reply to your hello.\n\n-- \nTaken from RFC 5322, section A.2.\n",
+    "epilogue": None,
+}
---
+++ test/data/emails/resent.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta, timezone
+
+data = {
+    "headers": {
+        "resent-from": [
+            {
+                "realname": "Mary Smith",
+                "address": "mary@example.net",
+            },
+        ],
+        "resent-to": [
+            {
+                "realname": "Jane Brown",
+                "address": "j-brown@other.example",
+            },
+        ],
+        "resent-date": [
+            datetime(1997, 11, 24, 14, 22, 1, tzinfo=timezone(timedelta(hours=-8))),
+        ],
+        "resent-message-id": [
+            "<78910@example.net>",
+        ],
+        "from": [
+            {
+                "realname": "John Doe",
+                "address": "jdoe@machine.example",
+            },
+        ],
+        "to": [
+            {
+                "realname": "Mary Smith",
+                "address": "mary@example.net",
+            },
+        ],
+        "subject": "Saying Hello",
+        "date": datetime(1997, 11, 21, 9, 55, 6, tzinfo=timezone(timedelta(hours=-6))),
+        "message-id": "<1234@local.machine.example>",
+    },
+    "preamble": None,
+    "content": "This is a message just to say hello.\nSo, \"Hello\".\n\n-- \nTaken from RFC 5322, section A.3.\n",
+    "epilogue": None,
+}
